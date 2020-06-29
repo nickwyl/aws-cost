862a3e1893ab3cf7ab1f7436abd1c3823dcdbcda
Nick Wang <ylw238@nyu.edu>
2020-06-29 13:46:33 -0400

    changed cost.go to not use pointers

--- cost.go
+++ cost.go
@@ -23,194 +23,196 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+"fmt"
+"os"
+"strconv"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/costexplorer"
-	"github.com/aws/aws-sdk-go/service/organizations"
+"github.com/aws/aws-sdk-go/aws"
+"github.com/aws/aws-sdk-go/aws/session"
+"github.com/aws/aws-sdk-go/service/costexplorer"
+"github.com/aws/aws-sdk-go/service/organizations"
 
-	"flag"
-	"time"
+"flag"
+"time"
 )
 
 func main() {
-	//measure time
-	startTime := time.Now()
+//measure time
+startTime := time.Now()
 
-	//Initialize a session with the osd-staging-1 profile or any user that has access to the desired info
-	sess, err := session.NewSessionWithOptions(session.Options{
-		Profile: "osd-staging-1",
-	})
-	if err != nil {
-		exitErrorf("Unable to generate session,", err)
-	}
+//Initialize a session with the osd-staging-1 profile or any user that has access to the desired info
+sess, err := session.NewSessionWithOptions(session.Options{
+Profile: "osd-staging-1",
+})
+if err != nil {
+exitErrorf("Unable to generate session,", err)
+}
 
-	// Create Cost Explorer Service Client
-	ce := costexplorer.New(sess)
-	//Accessing organizations
-	org := organizations.New(sess)
+// Create Cost Explorer Service Client
+ce := costexplorer.New(sess)
+//Accessing organizations
+org := organizations.New(sess)
 
-	//Get v4 organizational unit
-	v4 := organizations.OrganizationalUnit{
-		Id:   aws.String("ou-0wd6-aff5ji37"),
-		//Id:   aws.String("ou-0wd6-3321fxfw"), //Test small OU
-		//Id:   aws.String("ou-0wd6-k7wulboi"), //slightly larger small OU
-		//Id:   aws.String("r-0wd6"), //Test root
-	}
+//Get v4 organizational unit
+v4 := organizations.OrganizationalUnit{
+//Id:   aws.String("ou-0wd6-aff5ji37"),
+//Id:   aws.String("ou-0wd6-3321fxfw"), //Test small OU
+Id:   aws.String("ou-0wd6-k7wulboi"), //slightly larger small OU
+//Id:   aws.String("r-0wd6"), //Test root
+}
 
-	//Store cost of OU
-	var cost float64 = 0
+//Store cost of OU
+var cost float64 = 0
 
-	//Set flag pointers
-	rPtr := flag.Bool("r", false, "recurse")
-	recursivePtr := flag.Bool("recursive", false, "recurse")
-	timePtr := flag.String("time", "all", "set time")
-	//Parse pointers
-	flag.Parse()
+//Set flag pointers
+rPtr := flag.Bool("r", false, "recurse")
+recursivePtr := flag.Bool("recursive", false, "recurse")
+timePtr := flag.String("time", "all", "set time")
+//Parse pointers
+flag.Parse()
 
-	//If -r flag is present, do a DFS postorder traversal and get cost of all accounts under OU
-	if *rPtr || *recursivePtr {
-		DFS(&v4, org, ce, timePtr, &cost)
-	} else {	//Else, get cost of only immediate accounts under OU
-		getOUCost(&v4, org, ce, timePtr, &cost)
-	}
+//If -r flag is present, do a DFS postorder traversal and get cost of all accounts under OU
+if *rPtr || *recursivePtr {
+cost = DFS(&v4, org, ce, timePtr)
+} else {	//Else, get cost of only immediate accounts under OU
+cost = getOUCost(&v4, org, ce, timePtr)
+}
 
-	fmt.Println("Recursive cost of OU:",cost)
-
-	//End time
-	endTime := time.Now()
-	fmt.Println("Time of program execution:",endTime.Sub(startTime))
+fmt.Println("Recursive cost of OU:",cost)
+//End time
+endTime := time.Now()
+fmt.Println("Time of program execution:",endTime.Sub(startTime))
 }
 
 
 //Get cost of accounts from current OU and child OUs
-func accountCost(accountID *string, ce *costexplorer.CostExplorer, timePtr *string, cost *float64) {
-	//Values
-	start := strconv.Itoa(time.Now().Year()-1) + time.Now().Format("-01-") + "01"	//Starting from the 1st of the current month last year i.e. if today is 2020-06-29, then start date is 2019-06-01
-	end := time.Now().Format("2006-01-02")
-	granularity := "MONTHLY"
-	metrics := []string{
-		"NetUnblendedCost",
-	}
+func accountCost(accountID *string, ce *costexplorer.CostExplorer, timePtr *string) float64 {
+//Values
+start := strconv.Itoa(time.Now().Year()-1) + time.Now().Format("-01-") + "01"	//Starting from the 1st of the current month last year i.e. if today is 2020-06-29, then start date is 2019-06-01
+end := time.Now().Format("2006-01-02")
+granularity := "MONTHLY"
+metrics := []string{
+"NetUnblendedCost",
+}
 
-	switch *timePtr {
-	case "MTD":
-		start = time.Now().Format("2006-01") + "-01"
-		end = time.Now().Format("2006-01-02")
-	case "YTD":
-		start = time.Now().Format("2006") + "-01-01"
-		end = time.Now().Format("2006-01-02")
-	}
+switch *timePtr {
+case "MTD":
+start = time.Now().Format("2006-01") + "-01"
+end = time.Now().Format("2006-01-02")
+case "YTD":
+start = time.Now().Format("2006") + "-01-01"
+end = time.Now().Format("2006-01-02")
+}
 
-	//Get cost information for chosen account
-	result, err := ce.GetCostAndUsage(&costexplorer.GetCostAndUsageInput{
-		Filter: &costexplorer.Expression{
-			Dimensions: &costexplorer.DimensionValues{
-				Key: aws.String("LINKED_ACCOUNT"),
-				Values: []*string{
-					accountID,
-				},
-			},
-		},
-		TimePeriod: &costexplorer.DateInterval{
-			Start: aws.String(start),
-			End:   aws.String(end),
-		},
-		Granularity: aws.String(granularity),
-		Metrics: aws.StringSlice(metrics),
-	})
-	if err != nil {
-		exitErrorf("Unable to generate report, %v", err)
-	}
+//Get cost information for chosen account
+result, err := ce.GetCostAndUsage(&costexplorer.GetCostAndUsageInput{
+Filter: &costexplorer.Expression{
+Dimensions: &costexplorer.DimensionValues{
+Key: aws.String("LINKED_ACCOUNT"),
+Values: []*string{
+accountID,
+},
+},
+},
+TimePeriod: &costexplorer.DateInterval{
+Start: aws.String(start),
+End:   aws.String(end),
+},
+Granularity: aws.String(granularity),
+Metrics: aws.StringSlice(metrics),
+})
+if err != nil {
+exitErrorf("Unable to generate report, %v", err)
+}
 
-	//var totalCost float64 = 0
+var totalCost float64 = 0
 
-	//Loop through month-by-month cost to get total cost
-	for month := 0; month < len(result.ResultsByTime); month++ {
-		currentCost, err := strconv.ParseFloat(*result.ResultsByTime[month].Total["NetUnblendedCost"].Amount, 64)
-		if err != nil {
-			exitErrorf("Unable to get cost,", err)
-		}
-		//totalCost += cost
-		*cost += currentCost
-	}
+//Loop through month-by-month cost to get total cost
+for month := 0; month < len(result.ResultsByTime); month++ {
+cost, err := strconv.ParseFloat(*result.ResultsByTime[month].Total["NetUnblendedCost"].Amount, 64)
+if err != nil {
+exitErrorf("Unable to get cost,", err)
+}
+totalCost += cost
+}
+
+return totalCost
 }
 
 //Get cost of accounts from current OU
-func getOUCost(OU *organizations.OrganizationalUnit, org *organizations.Organizations, ce *costexplorer.CostExplorer, timePtr *string, cost *float64) {
-	//var cost float64 = 0
-	//Get accounts
-	accounts, err := org.ListAccountsForParent(&organizations.ListAccountsForParentInput{
-		ParentId:   OU.Id,
-	})
+func getOUCost(OU *organizations.OrganizationalUnit, org *organizations.Organizations, ce *costexplorer.CostExplorer, timePtr *string) float64 {
+var cost float64 = 0
+//Get accounts
+accounts, err := org.ListAccountsForParent(&organizations.ListAccountsForParentInput{
+ParentId:   OU.Id,
+})
 
-	//Populate accountSlice with accounts by looping until accounts.NextToken is null
-	for {
-		if err != nil {	//Look at this for error handling: https://docs.aws.amazon.com/sdk-for-go/api/service/organizations/#example_Organizations_ListOrganizationalUnitsForParent_shared00
-			exitErrorf("Unable to retrieve accounts under OU", err)
-		}
-
-		////Increment costs of accounts
-		for i := 0; i < len(accounts.Accounts); i++ {
-			accountCost(accounts.Accounts[i].Id, ce, timePtr, cost)
-		}
-
-		if accounts.NextToken == nil {
-			break
-		}
-
-		//Get accounts
-		accounts, err = org.ListAccountsForParent(&organizations.ListAccountsForParentInput{
-			ParentId:   OU.Id,
-			NextToken: accounts.NextToken,
-		})
-	}
+//Populate accountSlice with accounts by looping until accounts.NextToken is null
+for {
+if err != nil {	//Look at this for error handling: https://docs.aws.amazon.com/sdk-for-go/api/service/organizations/#example_Organizations_ListOrganizationalUnitsForParent_shared00
+exitErrorf("Unable to retrieve accounts under OU", err)
 }
 
-func DFS(OU *organizations.OrganizationalUnit, org *organizations.Organizations, ce *costexplorer.CostExplorer, timePtr *string, cost *float64) {
-	//var cost float64 = 0
-	var OUSlice []*organizations.OrganizationalUnit
+////Increment costs of accounts
+for i := 0; i < len(accounts.Accounts); i++ {
+cost += accountCost(accounts.Accounts[i].Id, ce, timePtr)
+}
 
-	//Get child OUs under parent OU
-	OUs, err := org.ListOrganizationalUnitsForParent(&organizations.ListOrganizationalUnitsForParentInput{
-		ParentId: OU.Id,
-	})
+if accounts.NextToken == nil {
+break
+}
 
-	//Populate OUSlice with OUs by looping until OUs.NextToken is null
-	for {
-		if err != nil {
-			exitErrorf("Unable to retrieve child OUs under OU", err)
-		}
+//Get accounts
+accounts, err = org.ListAccountsForParent(&organizations.ListAccountsForParentInput{
+ParentId:   OU.Id,
+NextToken: accounts.NextToken,
+})
+}
+return cost
+}
 
-		//Add OUs to slice
-		for childOU := 0; childOU < len(OUs.OrganizationalUnits); childOU++ {
-			OUSlice = append(OUSlice,OUs.OrganizationalUnits[childOU])
-		}
+func DFS(OU *organizations.OrganizationalUnit, org *organizations.Organizations, ce *costexplorer.CostExplorer, timePtr *string) float64 {
+var cost float64 = 0
+var OUSlice []*organizations.OrganizationalUnit
 
-		if OUs.NextToken == nil {
-			break
-		}
+//Get child OUs under parent OU
+OUs, err := org.ListOrganizationalUnitsForParent(&organizations.ListOrganizationalUnitsForParentInput{
+ParentId: OU.Id,
+})
 
-		OUs, err = org.ListOrganizationalUnitsForParent(&organizations.ListOrganizationalUnitsForParentInput{
-			ParentId:  OU.Id,
-			NextToken: OUs.NextToken,
-		})
-	}
+//Populate OUSlice with OUs by looping until OUs.NextToken is null
+for {
+if err != nil {
+exitErrorf("Unable to retrieve child OUs under OU", err)
+}
 
-	//Loop through all child OUs, get their costs, and store it to cost of current OU
-	for _,childOU := range OUSlice {
-		DFS(childOU, org, ce, timePtr, cost)
-	}
+//Add OUs to slice
+for childOU := 0; childOU < len(OUs.OrganizationalUnits); childOU++ {
+OUSlice = append(OUSlice,OUs.OrganizationalUnits[childOU])
+}
 
-	//Return cost of child OUs + cost of immediate accounts under current OU
-	getOUCost(OU, org, ce, timePtr, cost)
+if OUs.NextToken == nil {
+break
+}
+
+OUs, err = org.ListOrganizationalUnitsForParent(&organizations.ListOrganizationalUnitsForParentInput{
+ParentId:  OU.Id,
+NextToken: OUs.NextToken,
+})
+}
+
+//Loop through all child OUs, get their costs, and store it to cost of current OU
+for _,childOU := range OUSlice {
+cost += DFS(childOU, org, ce, timePtr)
+}
+
+//Return cost of child OUs + cost of immediate accounts under current OU
+return cost + getOUCost(OU, org, ce, timePtr)
 }
 
 func exitErrorf(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, msg+"\n", args...)
-	os.Exit(1)
+fmt.Fprintf(os.Stderr, msg+"\n", args...)
+os.Exit(1)
 }
+
 
